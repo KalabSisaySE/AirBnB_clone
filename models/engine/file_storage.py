7ad10df24ce90d6cbe5a671e5cd22d06d4a07fea
KalabSisaySE <kyimusic11@gmail.com>
2024-10-14 18:40:31 +0300

    feat(models): add new models to file storage and command interpreter

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,6 +30,11 @@ class FileStorage:
     def reload(self):
         """deserializes JSON file back to objects"""
         from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
         from models.user import User
 
         json_data = None
